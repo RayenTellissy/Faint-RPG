bc0d811cf3f836c3dfada82882de832512f3259f
RayenTellissy <rayentellissy571@gmail.com>
2023-04-10 00:34:17 +0100

    .

--- game.js
+++ game.js
@@ -40,51 +40,43 @@ class Stage1 extends React.Component{
     this.state={
       character: "Images/Character/standDown.png",
       characterX: 550,
-      characterY: 400
+      characterY: 400,
+      velocityRight: 8,
+      velocityLeft: -8,
+      velocityUp: -8,
+      velocityDown: 8
     }
   }
   componentDidMount(){
     // W event
     addEventListener("keydown", (event)=>{
       if(event.keyCode===87){
-        var velocityY=this.state.characterY-5
         this.setState({
           character: "Images/Character/walkUp.png",
-          characterY: velocityY
+          characterY: this.state.characterY+this.state.velocityUp
         })
       }
-    })
-    // A event
-    addEventListener("keydown", (event)=>{
+        // A event
       if(event.keyCode===65){
-        var velocityX=this.state.characterX-5
         this.setState({
           character: "Images/Character/walkLeft.png",
-          characterX: velocityX
+          characterX: this.state.characterX+this.state.velocityLeft
         })
       }
-    })
-    // S event
-    addEventListener("keydown", (event)=>{
+        // S event
       if(event.keyCode===83){
-        var velocityY=this.state.characterY+5
         this.setState({
           character: "Images/Character/walkDown.png",
-          characterY: velocityY
+          characterY: this.state.characterY+this.state.velocityDown
         })
       }
-      
-    })
-    // D event
-    addEventListener("keydown", (event)=>{
+       // D event
       if(event.keyCode===68){
-        var velocityX=this.state.characterX+5
         this.setState({
           character: "Images/Character/walkRight.png",
-          characterX: velocityX
+          characterX: this.state.characterX+this.state.velocityRight
         })
       }
-      
     })
   }
   render(){

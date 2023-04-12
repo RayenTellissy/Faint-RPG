54c1d7f89ce7b059471b778cb14de1df8e7212d8
RayenTellissy <rayentellissy571@gmail.com>
2023-04-12 18:00:44 +0100

    .

--- src/Components/Display.jsx
+++ src/Components/Display.jsx
@@ -4,7 +4,7 @@ import Character from "./Character.jsx"
 const Display=()=>{
   const [map,setMap]= useState("../../src/data/Maps/main/Map_First.png")
     return(
-      <div id="canvas" style={{backgroundImage: `url(${map})`}} onLoad={handleCanvas}>
+      <div id="canvas" style={{backgroundImage: `url(${map})`}}>
         <Character/>
         <img style={{height: "200px"}} src={"../../src/data/NPCs/Friendly_Xanafin.png"}></img>
       </div>

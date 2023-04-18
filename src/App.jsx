6d3b73f094f798d1ed310ee6cd694b3ef2e77cbd
RayenTellissy <rayentellissy571@gmail.com>
2023-04-18 11:47:30 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import React from "react"
+import React, { useEffect } from "react"
 import Stats from "./Components/Stats.jsx";
 import BugReport from "../src/Components/BugReport.jsx"
 import { ContextProvider } from "./Components/Context.jsx";
 import { Map } from "./Components/Map.jsx";
-
+import axios from "axios";
+const url="https://faintrpgbugreport.onrender.com/bugs"
 
 
 const App=()=>{
+/** 
+ *  ? : experimental get from bugreport server, to turn on server */
+  useEffect(()=>{
+    axios.get(url)
+  })
+
   return (
     <div>
       <ContextProvider>
